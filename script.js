6df8f00b9092fef7177f746b86323af4f0c92cd1
alyaapriliani846-ops <alyaapriliani846@gmail.com>
2026-05-07 21:26:00 +0700

    update design

--- script.js
+++ script.js
@@ -5,21 +5,15 @@ function showMessage(){
         "_blank"
     );
 }
-
 function copyEmail(){
     navigator.clipboard.writeText(
         "alyaapriliani846@gmail.com"
     );
-
     const btn = document.querySelector(".copy-btn");
-
     btn.innerHTML =
     "✓ Email Copied";
-
     setTimeout(() => {
-
         btn.innerHTML =
         '<i class="fa-solid fa-copy"></i> Copy Email';
-
     }, 2000);
 }
