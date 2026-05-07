3b694c89053938ec976e4c5d3d6a4ace8b8626aa
alyaapriliani846-ops <alyaapriliani846@gmail.com>
2026-05-07 17:46:03 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function showMessage(){
+
+    window.open(
+        "https://wa.me/6285136371361",
+        "_blank"
+    );
+}
+
+function copyEmail(){
+    navigator.clipboard.writeText(
+        "alyaapriliani846@gmail.com"
+    );
+
+    const btn = document.querySelector(".copy-btn");
+
+    btn.innerHTML =
+    "✓ Email Copied";
+
+    setTimeout(() => {
+
+        btn.innerHTML =
+        '<i class="fa-solid fa-copy"></i> Copy Email';
+
+    }, 2000);
+}
